236db0542c28ed8b76088f1962910b9712575d70
perfectionist <loeffler@mac.com>
2012-01-10 20:21:56 -0600

    Initial Commit

---
+++ Recipes/PRPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PRPAppDelegate.h
+//  Recipes
+//
+//  Created by David Loeffler on 1/10/12.
+//  Copyright (c) 2012 e-tuitive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PRPViewController;
+
+@interface PRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) PRPViewController *viewController;
+
+@end
---
+++ Recipes/PRPViewController.m
@@ -0,0 +1,34 @@
+//
+//  PRPViewController.m
+//  Recipes
+//
+//  Created by David Loeffler on 1/10/12.
+//  Copyright (c) 2012 e-tuitive. All rights reserved.
+//
+
+#import "PRPViewController.h"
+
+@interface PRPViewController ()
+
+@end
+
+@implementation PRPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
